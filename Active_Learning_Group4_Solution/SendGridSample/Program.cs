7efe2f558b9a96b15617376725f3e44a32ff01bd
smitabisoyi <Smita.bisoyi08@gmail.com>
2017-06-28 00:32:49 +0800

    Added template

--- Active_Learning_Group4_Solution/SendGridSample/Program.cs
+++ Active_Learning_Group4_Solution/SendGridSample/Program.cs
@@ -23,6 +23,22 @@ namespace SendGridSample
             var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
             var response = await client.SendEmailAsync(msg);
+            
+            //var apiKey = "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";
+            //var client = new SendGridClient(apiKey);
+
+            //List<EmailAddress> tos = new List<EmailAddress>();
+            //tos.Add(new EmailAddress("joechenghao@gmail.com", "Joe"));
+            //tos.Add(new EmailAddress("smita.bisoyi08@gmail.com", "Smita"));
+
+            //var msg = new SendGridMessage();
+            //msg.SetFrom(new EmailAddress("test@example.com", "CA Support Team"));
+            //msg.SetSubject("Testing SendGrid!");
+            //msg.AddTos(tos);
+            //msg.SetTemplateId("07afecb2-e07f-47cb-b383-775b7e87c7cf");
+            ////substitution to template Dear -NAME-.
+            //msg.AddSubstitution("-NAME-", "User");
+            //var response = await client.SendEmailAsync(msg);
         }
     }
 }
